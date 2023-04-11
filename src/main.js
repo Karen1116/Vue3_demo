1774bd666db256277440729a79b2675e9ffc1eb2
Karen1116 <2839915137@qq.com>
2023-04-11 09:00:33 +0800

    cancel

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Helloworld from './components/HelloWorld.vue'
 
 createApp(App).mount('#app')
-
-createApp(Helloworld).mount('#Helloworld')
