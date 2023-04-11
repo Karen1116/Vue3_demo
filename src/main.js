996deaaeeaadbe1577f951ee4d13c3ca5e860eca
Karen1116 <2839915137@qq.com>
2023-04-11 08:50:28 +0800

    App change to Helloworld

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Helloworld from './components/Helloworld.vue'
 
 createApp(App).mount('#app')
+
+createApp(Helloworld).mount('#Helloworld')
